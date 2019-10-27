e453cc6587b5646f0d67dae7a92ef2e50b9bbe4f
Mihai Gătejescu <gus666xe@gmail.com>
2019-10-27 18:33:43 +0200

    Update the notices in source files

--- src/cursmenu.h
+++ src/cursmenu.h
@@ -3,21 +3,22 @@
  *
  * Copyright (C) 2003, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015,
  *               2016, 2017, 2018, 2019
- *               GNU Typist Development Team <bug-gtypist@gnu.org>
+ *               Hynek Hanke, Paul Goins, Mihai Gătejescu
  *
- * This program is free software: you can redistribute it and/or modify
+ * GNU Typist is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * GNU Typist is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with GNU Typist.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #ifndef CURSMENU_H
 #define CURSMENU_H
 
