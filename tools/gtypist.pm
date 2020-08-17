b57e6fb16c529cdf27f4787c8e1f151a8421ee4e
Mihai Gătejescu <gus666xe@gmail.com>
2020-08-17 22:51:41 +0300

    Update the notices in tools/

--- tools/gtypist.pm
+++ tools/gtypist.pm
@@ -1,21 +1,26 @@
 #!/usr/bin/perl -w
-
 # functions to create a menu + jump-table for a set of gtypist-lessons
 # plus some miscellaneous functions to convert to gtypist-lessons
 
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation, either version 3
-# of the License, or (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
+# Copyright (C) 2001, 2002, 2003 Simon Baldwin (simonb@sco.com)
+# Copyright (C) 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
+#               2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
+#               2019, 2020 Felix Natter, Paul Goins
+
+# This file is past of GNU Typist
+
+# GNU Typist is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# GNU Typist is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# along with GNU Typist.  If not, see <http://www.gnu.org/licenses/>.
 
 use strict;
 package gtypist;
--- tools/tt2typ.pl
+++ tools/tt2typ.pl
@@ -1,22 +1,28 @@
 #!/usr/bin/perl -w
-
 # converts tipptrainer-0.3.3-lessons to gtypist's .typ-file
 # send comments and suggestions to bug-gtypist@gnu.org
 
-# Author: Felix Natter <fnatter@gmx.net> 2001-2011
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation, either version 3
-# of the License, or (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
+# Copyright (C) 2001, 2002, 2003 Simon Baldwin (simonb@sco.com)
+# Copyright (C) 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
+#               2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
+#               2019, 2020 Felix Natter, Paul Goins
+
+# Author: Felix Natter <fnatter@gmx.net>
+
+# This file is past of GNU Typist
+
+# GNU Typist is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# GNU Typist is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# along with GNU Typist.  If not, see <http://www.gnu.org/licenses/>.
 
 use strict;
 use Cwd; # Cwd::getcwd
