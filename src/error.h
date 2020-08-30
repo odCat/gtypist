7f4dbbb61f002e7d17aa8a8c9c462787c3fb2ed9
Mihai Gătejescu <gus666xe@gmail.com>
2020-08-30 18:22:46 +0300

    Fix indentation in copyright notices

--- src/error.h
+++ src/error.h
@@ -2,7 +2,7 @@
  * GNU Typist  - interactive typing tutor program for UNIX systems
  * 
  * Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003  
- * 					Simon Baldwin (simonb@sco.com)
+ * 				 Simon Baldwin (simonb@sco.com)
  * Copyright (C) 2003, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015,
  *               2016, 2017, 2018, 2019, 2020
  *               Hynek Hanke, Paul Goind, Mihai Gătejescu
